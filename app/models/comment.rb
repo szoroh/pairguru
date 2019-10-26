de7b8b9b20134be257fe282851932dfd758b2024
szoroh <kubahorosz95@gmail.com>
2019-10-26 04:25:40 +0200

    Add constraint that limits user to just one comment for a single movie

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
 
   validates :title, presence: true
   validates :body,  presence: true
+
+  validates :movie_id, uniqueness: { scope: :user_id, message: " can be commented only once by you."}
 end
